40238c396f6be5291b5844a99c88f04905add2ba
willh33 <wharrison33@hotmail.com>
2020-10-03 15:37:38 -0600

    commented code

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ let specialCheckbox = document.getElementById('special');
 let passwordParagraph = document.getElementById('password')
 let numericCheckbox = document.getElementById('numeric');
 
+//Whether we want to include the character set or not
 let includeLowercase = true;
 let includeUppercase = true;
 let includeNumeric = true;
 let includeSpecial = true;
 
+//Character sets
 let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 let lowercase = 'abcdefghijklmnopqrstuvwxyz';
 let numbers = '0123456789';
@@ -24,15 +26,14 @@ function writePassword() {
   let passwordText = document.querySelector("#password");
 
   passwordText.textContent = password;
-
+  passwordText.select();
+  document.execCommand("copy");
+  alert("Password has been copied to clipboard!");
 }
 
 function generatePassword() {
   //Get the checkbox values to decide what to include in the password
-  includeLowercase = lowerCaseCheckbox.checked;
-  includeUppercase = uppercaseCheckbox.checked;
-  includeNumeric = numericCheckbox.checked;
-  includeSpecial = specialCheckbox.checked;
+  setCheckboxValues();
 
   //At least one has to be checked
   if(!includeLowercase && !includeNumeric && !includeUppercase && !includeSpecial){
@@ -41,15 +42,7 @@ function generatePassword() {
   }
 
   //create an array of the valid character sets
-  let validCharacterSets = [];
-  if(includeLowercase)
-    validCharacterSets.push(lowercase);
-  if(includeUppercase)
-    validCharacterSets.push(uppercase);
-  if(includeNumeric)
-    validCharacterSets.push(numbers);
-  if(includeSpecial)
-    validCharacterSets.push(specialCharacters);
+  let validCharacterSets = getValidCharacterSets();
 
   //Get the number of characters
   let numCharacters = numCharactersEl.value;
@@ -65,8 +58,11 @@ function generatePassword() {
   //each time create a random character from a random set of the included characters 
   for(let i = 0; i < numCharacters - 1; i++)
   {
+    //Get a random character set to get this character from
     let characterSet = validCharacterSets[Math.floor(Math.random() * validCharacterSets.length)];
+    //Get a random character from the random character set
     let character = characterSet[Math.floor(Math.random() * characterSet.length)];
+    //Add the character to password
     password += character;
   }
 
@@ -75,3 +71,29 @@ function generatePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Set the checkbox values, each character type we want to include is eithe true or false
+function setCheckboxValues() {
+  includeLowercase = lowerCaseCheckbox.checked;
+  includeUppercase = uppercaseCheckbox.checked;
+  includeNumeric = numericCheckbox.checked;
+  includeSpecial = specialCheckbox.checked;
+}
+
+/**
+ * Using the variable for each character set (set to true or false)
+ * return an array with each character set we want to include in our password 
+ * */
+function getValidCharacterSets() {
+  let validCharacterSets = [];
+  if(includeLowercase)
+    validCharacterSets.push(lowercase);
+  if(includeUppercase)
+    validCharacterSets.push(uppercase);
+  if(includeNumeric)
+    validCharacterSets.push(numbers);
+  if(includeSpecial)
+    validCharacterSets.push(specialCharacters);
+
+  return validCharacterSets;
+}
